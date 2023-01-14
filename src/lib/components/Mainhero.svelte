a6825042fd073f727b447b0541492b07ef4bc653
Shauri Maigua <90189750+SCNMC@users.noreply.github.com>
2023-01-14 18:42:29 +0100

    Made header and hero for desktop

--- src/lib/components/Mainhero.svelte
+++ src/lib/components/Mainhero.svelte
@@ -1,22 +1,114 @@
 <section>
-<h2>FRONTEND DESIGN<br>& <span>DEVELOPMENT</span></h2>
-<p>WE LOVE WEB</p>
+	<img src="/images/weloveweb.jpeg" alt="fdnd" />
+	<h2>FRONTEND DESIGN<br />& <span>DEVELOPMENT</span></h2>
 </section>
+
+<div>
+	<p>WE</p><span>♥</span><p>THE</p>
+	<p> WEB </p>
+</div>
+
 <style>
-
-section{
-
-    display: flex;
-    flex-direction: column;
-    z-index: -1;
-    padding: 2em 1em;
-    }
-    h2{
+	section {
+		display: flex;
+		flex-direction: column;
+		color: white;
+		padding: 3em 1em;
+		gap: 2em;
+		line-height: 3.5em;
+	}
+	/* h2{
         font-size: 2.5em;
         font-weight: bold;
-    }
+    } */
+	img {
+		width: 100%;
+	}
+	h2 {
+		width: fit-content;
+		font-size: 2em;
+		background-color: #050542;
+		border-top-right-radius: 15px;
+		margin-top: -3em;
+		padding-right: 0.5em;
+	}
+	div {
+		display: flex;
+		align-items: center;
+		gap: 2em;
+		padding: 1em;
+	}
+	div :nth-child(1) {
+		color: #050542;
+		padding: 1em;
+		background-color: #a675f5;
+		font-weight: bold;
+	}
+	div :nth-child(2) {
+		text-align: center;
+		background-color: #66e5bf;
+		font-size: 2.5rem;
+		border-radius: 2rem;
+		width: 4rem;
+	}
+	div :nth-child(3) {
+		color: white;
+		border: 2px solid white;
+		border-radius: 2rem;
+		padding: 1em;
+	}
+	div :nth-child(4) {
+		background-color: #a675f5;
+		padding: 1em;
+		width: 5rem;
+		border-radius: 2rem;
+		text-align: center;
+	}
 
-    p{
-padding-top: 3em;
-    }
+	@media (min-width: 25rem) {
+		img {
+			height: 25em;
+		}
+	}
+
+	@media (min-width: 60rem) {
+		p {
+			font-size: 2.5em;
+		}
+		img {
+			height: 35rem;
+			object-fit: cover;
+		}
+		section {
+			padding: 2em 4em;
+		}
+		div {
+			padding-left: 4em;
+			justify-content: start;
+			gap: 4em;
+		}
+
+		div :nth-child(1) {
+			padding: 0.4em;
+		}
+		div :nth-child(2) {
+			font-size: 4.5rem;
+			border-radius: 4rem;
+			width: 6.5rem;
+		}
+		div :nth-child(3) {
+			padding: 1rem;
+			border-radius: 7rem;
+		}
+		div :nth-child(4) {
+			padding: 0.4em;
+			width: 20rem;
+			border-radius: 4em;
+		}
+		h2 {
+			font-size: 5em;
+			margin-top: -3em;
+			line-height: 1.4em;
+		}
+	}
 </style>
--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -1,5 +1,5 @@
 <header>
-    <h1>FDND</h1>
+    <h1>fdnd</h1>
     <nav>
         <ul>
             <li>Agenda</li>
@@ -10,13 +10,19 @@
 
 <style>
 h1{
+font-size: 2.5em;
+color: #66e5bf;
+background: #050542;
+box-shadow: -4px 4px 0px #66e5bf;
+border: solid 3px #66e5bf;
+border-radius: 9px;
 
 }
  header{
     display: flex;
     align-items: center;
     justify-content: space-between;
-    padding: 2em 1em;
+    padding: 3em 1em;
  }
 
  ul{
@@ -27,10 +33,32 @@ h1{
 
  li{
     text-align: center;
-    background-color: #050542;
-    color: white;
-    padding: .6em;
+    background-color:#66e5bf;;
+    color: #050542;
+    padding: .5em;
     border-radius: 1em;
-
+    font-size: 1em;
  }
+ ul :nth-child(2){
+    background-color: #050542;
+    border: 2px solid #66e5bf;
+    color: #66e5bf;
+	border-radius: 1em;
+ }
+
+ @media (min-width:60rem){
+    header{
+        padding: 2em 4em;
+    }
+    ul{
+        gap: 2em;
+    }
+    li{
+    padding: .6em;
+    border-radius: 2em;
+    width: 7em;
+ }
+
+
+}
 </style>
